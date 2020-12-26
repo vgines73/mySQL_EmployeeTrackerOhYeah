5a6014bfbadff3d3f43f8b12cb00d302dd7af590
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-12-26 10:52:43 -0800

    decided to go back to how I originally wanted to do it.

--- employee_tracker.sql
+++ employee_tracker.sql
@@ -23,6 +23,48 @@ CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY(id)
+);
+
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Vincent", "Gines", 1, NULL);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (2, "Michael", "Jordan", 3, 1);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (3, "Kobe", "Bryant", 3, 1);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (4, "Lebron", "James", 4, 1);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (5, "Kevin", "Durant", 4, 1);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (6, "Stephen", "Curry", 2, 1);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (7, "Allen", "Iverson", 2, 1);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (8, "Trae", "Young", 2, 1);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (9, "Luka", "Doncic", 2, 1);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (10, "Wilt", "Chamberlain", 6, 1);
+
+
+
+INSERT INTO role(id, title, salary, department_id)
+VALUES (1, "Manager", 50000, 1);
+INSERT INTO role(id, title, salary, department_id)
+VALUES (2, "Point Guard", 50000, 2);
+INSERT INTO role(id, title, salary, department_id)
+VALUES (3, "Shooting Guard", 50000, 2);
+INSERT INTO role(id, title, salary, department_id)
+VALUES (4, "Small Forward", 50000, 2);
+INSERT INTO role(id, title, salary, department_id)
+VALUES (5, "Power Forward", 50000, 2);
+INSERT INTO role(id, title, salary, department_id)
+VALUES (6, "Center", 50000, 2);
+
+INSERT INTO department(name)
+VALUES ("Management");
+INSERT INTO department(name)
+VALUES ("Player");
 
 SELECT * FROM employee;
 SELECT * FROM role;
--- seed.sql
+++ seed.sql
@@ -1,20 +1,42 @@
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES ("Vincent", "Gines", 1, NULL)
+USE employee_tracker_db;
 
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES ("Michael", "Jordan", 1, NULL)
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Vincent", "Gines", 1, NULL);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (2, "Michael", "Jordan", 3, 1);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (3, "Kobe", "Bryant", 3, 1);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (4, "Lebron", "James", 4, 1);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (5, "Kevin", "Durant", 4, 1);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (6, "Stephen", "Curry", 2, 1);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (7, "Allen", "Iverson", 2, 1);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (8, "Trae", "Young", 2, 1);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (9, "Luka", "Doncic", 2, 1);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (10, "Wilt", "Chamberlain", 6, 1);
 
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES ("Kobe", "Bryant", 1, NULL)
 
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES ("Lebron", "James", 1, NULL)
 
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES ("Kevin", "Durant", 1, NULL)
+INSERT INTO role(id, title, salary, department_id)
+VALUES (1, "Manager", 50000, 1);
+INSERT INTO role(id, title, salary, department_id)
+VALUES (2, "Point Guard", 50000, 2);
+INSERT INTO role(id, title, salary, department_id)
+VALUES (3, "Shooting Guard", 50000, 2);
+INSERT INTO role(id, title, salary, department_id)
+VALUES (4, "Small Forward", 50000, 2);
+INSERT INTO role(id, title, salary, department_id)
+VALUES (5, "Power Forward", 50000, 2);
+INSERT INTO role(id, title, salary, department_id)
+VALUES (6, "Center", 50000, 2);
 
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES ("Stephen", "Curry", 1, NULL)
-
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES ("Allen", "Iverson", 1, NULL)
+INSERT INTO department(name)
+VALUES ("Management");
+INSERT INTO department(name)
+VALUES ("Player");
